3806d511a9a2889caa16f9a45f7b8597a47b07eb
marwa <marwa@LAPTOP-MB>
2025-10-21 18:41:33 +0200

    added 1 persoon dragen

--- InfernoRescue/.github/copilot-instructions.md
+++ InfernoRescue/.github/copilot-instructions.md
@@ -1,15 +1,8 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
-
 - Project name: InfernoRescue
 - Unity version: Unity 6000.2.6f2
-- Active scene:
-  - Name: Jonathan
-  - Tags:
-    - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController, Fire, Ground
-  - Layers:
-    - Default, TransparentFX, Ignore Raycast, Ground, Water, UI, Interact
 - Active game object:
-  - Name: player
-  - Tag: Player
-  - Layer: Default
-  <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
+  - Name: Cursor
+  - Tag: Untagged
+  - Layer: UI
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- InfernoRescue/Assets/Scripts/FolloScripts/PlayerInteraction.cs
+++ InfernoRescue/Assets/Scripts/FolloScripts/PlayerInteraction.cs
@@ -5,6 +5,7 @@ public class PlayerInteraction : MonoBehaviour
     public Camera playerCamera;
     public float interactRange = 10f;
     public LayerMask interactLayer;
+    private Follower currentFolower;
     private void Start()
     {
         // Verberg en vergrendel de cursor
@@ -26,8 +27,20 @@ public class PlayerInteraction : MonoBehaviour
                 Follower npc = hit.collider.GetComponent<Follower>();
                 if (npc != null)
                 {
+                    if(currentFolower!=null && currentFolower != npc)
+                    {
+                        currentFolower.isFollowing = false;
+                        currentFolower.StopFollowing();
+                    }
                     npc.ToggleFollow();
-                    npc.player = transform; // speler doorgeven
+                    npc.player = transform;
+
+                    if (npc.isFollowing)
+                        currentFolower = npc;
+                    else if (currentFolower == npc)
+                        currentFolower = null;
+
+                       
                 }
             }
         }
