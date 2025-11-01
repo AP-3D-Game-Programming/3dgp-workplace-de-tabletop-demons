00ba4848f9f87392e11931211b4ad68b3a95ca61
LowieCasteels <lowie.casteels@gmail.com>
2025-11-01 15:27:42 +0100

    Extinguish Fire works

--- InfernoRescue/Assets/Scripts/ExtinguishFire.cs
+++ InfernoRescue/Assets/Scripts/ExtinguishFire.cs
@@ -8,18 +8,23 @@ public class ExtinguishFire : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            Debug.Log("E Pressed");
             Extinguish();
         }
     }
 
     void Extinguish()
     {
+        Debug.Log($"Extinguish attempt at position {transform.position} with range {range}");
         Collider[] hits = Physics.OverlapSphere(transform.position, range);
+        Debug.Log($"Number of colliders found: {hits.Length}");
 
         foreach (Collider hit in hits)
         {
+            Debug.Log($"Hit object: {hit.name}, Tag: {hit.tag}");
             if (hit.CompareTag("Fire"))
             {
+                Debug.Log($"Fire detected: {hit.name}. Destroying...");
                 Destroy(hit.gameObject);
                 Debug.Log("Vuur geblust");
             }
