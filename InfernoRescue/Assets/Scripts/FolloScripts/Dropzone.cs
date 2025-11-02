1256f9804cc26aad1473e961b0a8fe78b92e1a5a
marwa <marwa@LAPTOP-MB>
2025-11-02 17:16:45 +0100

    Partical effect en soud effect dropzone

--- InfernoRescue/Assets/Scripts/FolloScripts/Dropzone.cs
+++ InfernoRescue/Assets/Scripts/FolloScripts/Dropzone.cs
@@ -2,6 +2,30 @@ using UnityEngine;
 
 public class Dropzone : MonoBehaviour
 {
+    public GameObject vfxEffect; // verwijzing naar het Particle System object
+    public AudioSource audioSource; // AudioSource voor het geluid
+    private ParticleSystem particleSystem;
+
+    private void Start()
+    {
+        if (vfxEffect != null)
+        {
+            particleSystem = vfxEffect.GetComponent<ParticleSystem>();
+            if (particleSystem == null)
+            {
+                Debug.LogWarning("Geen ParticleSystem component gevonden op het opgegeven object.");
+            }
+            else
+            {
+                particleSystem.Stop(); // zorg dat het effect niet automatisch afspeelt
+            }
+        }
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+
+
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Follower npc = other.GetComponent<Follower>();
@@ -9,6 +33,18 @@ public class Dropzone : MonoBehaviour
         {
             npc.StopFollowing();
             Debug.Log("NPC afgeleverd in dropzone!");
+
+            // Speel het Particle System af
+            if (particleSystem != null)
+            {
+                particleSystem.Play();
+            }
+
+            // Speel het geluid af
+            if (audioSource != null)
+            {
+                audioSource.Play();
+            }
         }
     }
 }
